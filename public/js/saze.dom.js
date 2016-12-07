28ed67ebd695148d01079ed67f10093533aff426
arthurmyr <arthurmeyer12@gmail.com>
2016-12-07 13:23:43 +0100

    Clean dev branch

--- public/js/saze.dom.js
+++
@@ -1,8 +0,0 @@
-var saze = saze || {};
-
-// All the interactions with the html dom
-saze.dom = {};
-
-saze.dom.init = function(){
-  
-};
--- public/js/saze.init.js
+++
@@ -1,7 +0,0 @@
-var saze = saze || {};
-
-// All the initialisation of components
-saze.init = function(){
-  saze.dom.init();
-  if(saze.jplayer !== undefined) saze.jplayer.init();
-}
